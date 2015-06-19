bacf39b16e3c7119cb706a4298ede611da2852d9
Igor Ladkin <dgkplan@googlemail.com>
2015-06-19 15:49:26 +0300

    Add editing of description for bands.details.

--- app/routes/band/details.js
+++ app/routes/band/details.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    willTransition: function(transition) {
+      var controller = this.controller;
+      var leave;
+
+      if (controller.isEditing) {
+        leave = window.confirm('You have unsaved changes. Are you sure you want to leave?');
+
+        if (leave) {
+          controller.set('isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    }
+  }
 });
