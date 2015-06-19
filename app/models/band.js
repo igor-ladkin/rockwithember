f85711ba16c23b2f4ae328cd90d11d341a9c3513
Igor Ladkin <dgkplan@googlemail.com>
2015-06-19 21:03:58 +0300

    Edit models to use REST adapter.

--- app/models/band.js
+++ app/models/band.js
@@ -1,16 +1,7 @@
-import Ember from 'ember';
+import DS from 'ember-data'; 
 
-export default Ember.Object.extend({
-  name: '',
-  description: '',
-
-  setupSongs: function() {
-    if (!this.get('songs')) {
-      this.set('songs', []);
-    }
-  }.on('init'),
-
-  slug: function() {
-    return this.get('name').dasherize();
-  }.property('name'),
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  songs: DS.hasMany('song')
 });
