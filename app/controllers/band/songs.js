426dbe07de7e788bc1ad7cb705e877624cfb666b
Igor Ladkin <dgkplan@googlemail.com>
2015-06-19 11:04:29 +0300

    Add changes to UI. Add event handlers on submit to forms.
    Atfer creating a new band user automatically goes to band.songs route.
    If the band has no songs yet user will see a panel with a invite to create one.
    Form for creation song and band now react both on button click and submit with pressing return.

--- app/controllers/band/songs.js
+++ app/controllers/band/songs.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
+
   isAddButtonDisabled: function() {
     return Ember.isEmpty(this.get('title'));
-  }.property('title')
+  }.property('title'),
+
+  canCreateSong: function() {
+    return this.get('songCreationStarted') || this.get('model.length');
+  }.property('songCreationStarted', 'model.length'),
+
+  actions: {
+    enableSongCreation: function() {
+      return this.set('songCreationStarted', true);
+    }
+  }
 });
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -52,6 +52,7 @@ export default Ember.Route.extend({
       var band = Band.create({ name: name });
       bands.pushObject(band);
       this.get('controller').set('name', '');
+      this.transitionTo('band.songs', band);
     }
   }
 });
