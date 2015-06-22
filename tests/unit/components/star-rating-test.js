5a93dc609aad2e51c13dfd1e4c9e9397cc88f0f7
Igor Ladkin <dgkplan@googlemail.com>
2015-06-22 14:50:49 +0300

    Add unit tests for star component.

--- tests/unit/components/star-rating-test.js
+++ tests/unit/components/star-rating-test.js
@@ -1,19 +1,29 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('star-rating', 'Unit | Component | star rating', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar'],
   unit: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(2);
+test('Renders the full and empty stars correctly', function(assert) {
+  assert.expect(4);
 
-  // Creates the component instance
   var component = this.subject();
-  assert.equal(component._state, 'preRender');
 
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+  Ember.run(function() {
+    component.setProperties({
+      rating: 4,
+      maxRating: 5
+    });
+  });
+
+  assert.equal(this.$().find('.glyphicon-star').length, 4, 'The right amount of full stars is rendered');
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 1, 'The right amount of empty stars is rendered');
+
+  Ember.run(function() {
+    component.set('maxRating', 10);
+  });
+
+  assert.equal(this.$().find('.glyphicon-star').length, 4, 'The right amount of full stars is rendered after changing maxRating');
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 6, 'The right amount of empty stars is rendered after changing maxRating');
 });
